5841b0ac0e05c9f3450994dcb85f4b4d87fee9a0
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-08-30 17:23:23 +0100

    dumping and loading a png file works perfectly

--- code/zmq/core/manage.py
+++ code/zmq/core/manage.py
@@ -1,25 +1,38 @@
 __metaclass__ = type
 
+from os import path
+
 import zmq
+
+CUR_DIR = path.dirname(__file__)
+IMAGE = path.join(CUR_DIR, 'zeromq.png')
+DELIMITER = ';'
+DATA_DEL = ':DATA_START:'
 
 
 class ImageMessage:
-    def __init__(self, width, length, row, column, image):
+    def __init__(self, width, length, row, column, image_path):
         self.width = width
         self.length = length
         self.row = row
         self.column = column
-        self.image = image
+        self.image_path = image_path
+
+    def __str__(self):
+        return DELIMITER.join(map(str, [self.width, self.length, self.row, self.column, self.image_path]))
 
     def dump(self):
         """Dump the image to text mode
         """
+        return DATA_DEL.join([str(self), open(self.image_path).read()])
 
     @classmethod
     def load(cls, image_text):
         """Load the image from text
         """
-        
+        fields, data = image_text.split(DATA_DEL)
+        img = ImageMessage(*(fields.split(DELIMITER)))
+        open(img.image_path + '.loaded', 'w').write(data)
 
 
 def main():
--- code/zmq/core/tests/test_manage.py
+++ code/zmq/core/tests/test_manage.py
@@ -1,5 +1,10 @@
 import unittest
 
+from .. import manage
+
 
 class TestImageMessage(unittest.TestCase):
-    pass
+    def test_dump_and_load(self):
+        img = manage.ImageMessage(1, 1, 0, 0, manage.IMAGE)
+        dumped = img.dump()
+        manage.ImageMessage.load(dumped)
