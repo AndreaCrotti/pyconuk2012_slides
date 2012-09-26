86c3dff4440c414fe743eb050a898b233f675fd0
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-09-26 23:33:17 +0100

    add working push/pull and slides about it

---
+++ code/zmq/push_pull/pull.py
@@ -0,0 +1,19 @@
+import zmq
+import sys
+
+ADDR = 'tcp://127.0.0.1:9999'
+
+
+def pull(idx):
+    context = zmq.Context()
+    pull_sock = context.socket(zmq.PULL)
+    pull_sock.connect(ADDR)
+
+    while True:
+        msg = pull_sock.recv()
+        print("Worker %d consumed %s" % (idx, msg))
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    pull(int(sys.argv[1]))
