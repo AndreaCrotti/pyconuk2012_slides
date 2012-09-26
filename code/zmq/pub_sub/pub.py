1e43a2bd8f45294c99e883630e9665610cbec8ef
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-09-26 23:18:16 +0100

    fix publish and subscribe

--- code/zmq/pub_sub/pub.py
+++ code/zmq/pub_sub/pub.py
@@ -1,3 +1,19 @@
+from time import sleep
+from itertools import count
 import zmq
 
-ADDR 
+ADDR = 'tcp://127.0.0.1:9999'
+
+
+def pub():
+    context = zmq.Context()
+    pub_sock = context.socket(zmq.PUB)
+    pub_sock.bind(ADDR)
+
+    for i in count(0):
+        pub_sock.send(str(i))
+        sleep(2)
+
+
+if __name__ == '__main__':
+    pub() 
