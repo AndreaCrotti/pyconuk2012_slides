c17ac66be998c9bf0b4dc2bb36b7aed23c8a906a
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-09-30 22:11:08 +0100

    remove unused examples

--- code/zmq/web_interface/web_zmq.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, session, g, render_template, flash
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-
